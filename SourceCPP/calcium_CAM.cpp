7fe4f0c1cccea4e873552ab154b20dcca6ccea6c
laura-dangelo <laura.dangelo@live.com>
2020-11-02 16:09:45 -0800

    versione finale

--- SourceCPP/calcium_CAM.cpp
+++ SourceCPP/calcium_CAM.cpp
@@ -53,7 +53,7 @@ double logpost_gamma(const arma::vec& y, const arma::vec& cc, const arma::vec& A
 // prior on A for non-zero values
 /*
  * A ~ G0 = Gamma(hyp_A1, hyp_A2) 
- * la media è hyp_A1/hyp_A2
+ * the mean is hyp_A1/hyp_A2
  */
 double logprior_A(double & A, double & hyp_A1, double & hyp_A2) 
 {
@@ -63,7 +63,7 @@ double logprior_A(double & A, double & hyp_A1, double & hyp_A2)
 }
 
 // log-posterior on A
-// la funzione prende in input solo le y_i t.c. A_i = A_k per un certo k = 1,..., K (fissato).
+// input of the function are the y_i s.t. A_i = A_k for k = 1,..., K (fixed).
 double logpost_A(const arma::vec& y, const arma::vec& cc, double & A, 
                  double & b, double & gamma, double & sigma2, double & tau2, 
                  double & hyp_A1, double & hyp_A2)
@@ -88,10 +88,10 @@ double sample_mix(double & p, double & hyp_A1, double & hyp_A2)
 
 
 /* 
- * funzioni per slice sampler
+ * functions for slice sampler
  */
 
-// calcola coefficienti xi_D = (xi_1, xi_2, ...)
+// compute coefficients xi_D = (xi_1, xi_2, ...)
 arma::vec fun_xi_D(double & kappa_D, int & max_xiK)
 {
   arma::vec out(max_xiK);
@@ -102,7 +102,7 @@ arma::vec fun_xi_D(double & kappa_D, int & max_xiK)
   return(out) ;
 }
 
-// calcola coefficienti xi_O = (xi_1, xi_2, ...)
+// compute coefficients xi_O = (xi_1, xi_2, ...)
 arma::vec fun_xi_O(double & kappa_O, int & max_xiL)
 {
   arma::vec out(max_xiL);
@@ -113,7 +113,7 @@ arma::vec fun_xi_O(double & kappa_O, int & max_xiL)
   return(out) ;
 }
 
-// calcola pesi costruiti con stick breaking process a partire dal vettore di variabili Beta(a_k,b_k)
+// compute stick-breaking weights starting from the vector of beta r.v. Beta(a_k,b_k)
 arma::vec stick_breaking(arma::vec beta_var)
 {
   int len = beta_var.n_elem ;
@@ -132,7 +132,6 @@ arma::vec stick_breaking(arma::vec beta_var)
 /*
  * Slice sampler
  */
-// [[Rcpp::export]]
 Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g, 
                           arma::vec clusterD, arma::vec clusterO, 
                           const arma::vec& cc,
@@ -147,8 +146,8 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
                           int & check)
 {
   /*
-   * clusterD (lunghezza = J) allocazione del cluster sulle distribuzioni assume valori in {1,2,3,...}
-   * clusterO (lunghezza = T) allocazione del cluster delle osservazioni assume valori in {0,1,2,3,...}, dove 0 indica lo spike
+   * clusterD (lunghezza = J) cluster allocation on the distributions: takes value in {1,2,3,...}
+   * clusterO (lunghezza = T) cluster allocation on the observations: takes value in {0,1,2,3,...}, where 0 is a non-spike
    * 
    * xi_O = (xi^O_1, xi^O_2, ...)
    * xi_D = (xi^D_1, xi^D_2, ...)
@@ -175,8 +174,9 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
   }
   arma::vec maxK_j(J) ;
   maxK_j = 1 + arma::floor( (log(u_D) - log(1 - kappa_D)) / log(kappa_D) ) ;
-  int maxK = std::max(clusterD.max(), maxK_j.max()) ; // upper bound su numero di distribuzioni
+  int maxK = std::max(clusterD.max(), maxK_j.max()) ; // upper bound on the number of distributions
 
+  
   // step 2: sample latent uniform on the observations
   for(int t = 0; t < T; t++)
   {
@@ -184,7 +184,8 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
   } 
   arma::vec maxL_t(T) ;
   maxL_t = 1 + arma::floor( (log(u_O) - log(1 - kappa_O)) / log(kappa_O) ) ;
-  int maxL = std::max(clusterO.max(), maxL_t.max()) ; // upper bound su numero di parametri per ogni distribuzione
+  int maxL = std::max(clusterO.max(), maxL_t.max()) ; // upper bound on the number of clusters on the observations
+ 
   
   // step 3: sample the stick-breaking weights on the distributions
   arma::vec v_k(maxK) ;
@@ -199,6 +200,7 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
     v_k(k - 1) = R::rbeta(a_k, b_k) ;
   }
   pi_k = stick_breaking( v_k ) ;
+
   
   // step 4: sample the stick-breaking weights on the observations
   // for each k in {1,...,maxK} we have maxL weights: matrix(maxL, maxK)
@@ -227,10 +229,13 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
     }
      omega_lk.col(k-1) = stick_breaking( v_lk ) ;
   }
-
+  
+  
   // step 5: sample the distributional cluster indicator
   NumericVector probK(maxK) ;
   IntegerVector clusterD_id =  Rcpp::seq(1, maxK);
+  
+  
   for(int j = 0; j < J; j++)
   {
     arma::uvec ind_t = find(g == (j+1)) ;
@@ -253,8 +258,7 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
     probK =  exp(probK)  ;
     clusterD(j) = Rcpp::sample(clusterD_id, 1, false, probK)[0] ;
   }
-
- 
+  
 
   // step 6: sample the observational cluster indicator
   NumericVector probL(maxL) ;
@@ -273,6 +277,7 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
     clusterO(t) = Rcpp::sample(clusterO_id, 1, false, probL)[0] ;
     if( A(clusterO(t)) == 0 ) { clusterO(t) = 0 ; }
   }
+
   
   // step 7: sample the cluster parameters
   for(int l = 1; l < maxL; l++)
@@ -301,6 +306,7 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
       A(l) = sample_mix(p, hyp_A1, hyp_A2) ;
     }
   }
+ 
       
   return Rcpp::List::create(Rcpp::Named("clusterO") = clusterO,
                             Rcpp::Named("clusterD") = clusterD,
@@ -308,7 +314,7 @@ Rcpp::List slice_sampler(const arma::vec& y, const arma::vec& g,
 }
 
 
-// Gibbs sampler: funzione principale
+// Gibbs sampler: main function
 // [[Rcpp::export]]
 Rcpp::List calcium_gibbs(int Nrep, 
                          arma::vec y, arma::vec g,
@@ -356,7 +362,7 @@ Rcpp::List calcium_gibbs(int Nrep,
   out_tau2(0) = tau2_start ;
   out_p(0) = p_start ;
   
-  // init kalman filter quantities
+  // kalman filter quantities
   out_c.col(0) = cal;
   arma::vec filter_mean(n+1) ; // 0 1 ... n
   arma::vec filter_var(n+1) ; // 0 1 ... n
@@ -385,6 +391,7 @@ Rcpp::List calcium_gibbs(int Nrep,
   bool display_progress = true ;
   Progress p(Nrep, display_progress) ;
   
+  //// Main loop ////
   for(int i = 0; i < Nrep -1 ; i++)
   {
     if( Progress::check_abort() ) { return -1.0 ; }
@@ -420,7 +427,6 @@ Rcpp::List calcium_gibbs(int Nrep,
       
       out_c(j, i+1) = R::rnorm(back_mean, back_var) ;
     }
-    // out_c.col(i+1) = out_c.col(i) ;
     
     
     /*
@@ -428,10 +434,8 @@ Rcpp::List calcium_gibbs(int Nrep,
      */
     arma::vec z(n) ; 
     for(int j = 0; j < n; j++) { z(j) = y(j) - out_c(j+1, i+1) ; } 
-    
     out_b(i+1) = R::rnorm( (hyp_b2 * hyp_b1 + 1/out_sigma2(i) * arma::accu(z)) / (hyp_b2 + n / out_sigma2(i)) , 
           std::sqrt( 1/ (hyp_b2 + n / out_sigma2(i)) ) ) ;
-    // out_b(i+1) = out_b(i) ;
     
     
     /*
@@ -439,9 +443,7 @@ Rcpp::List calcium_gibbs(int Nrep,
      */
     arma::vec sq(n) ; 
     for(int j = 0; j < n; j++) { sq(j) = (z(j) - out_b(i+1)) * (z(j) - out_b(i+1)) ; }
-    
     out_sigma2(i+1) = 1/ R::rgamma(hyp_sigma21 + n/2, 1/(hyp_sigma22 + 0.5 * arma::accu(sq)) ) ;
-    // out_sigma2(i+1) = out_sigma2(i) ;
     
     
     /*
@@ -451,7 +453,6 @@ Rcpp::List calcium_gibbs(int Nrep,
     for(int j = 0; j < n ; j++) { sq2(j) = (out_c(j+1,i+1) - out_gamma(i) * out_c(j,i+1) - AA(j+1)) * 
        (out_c(j+1,i+1) - out_gamma(i) * out_c(j,i+1) - AA(j+1)) ; }
     out_tau2(i+1) = 1/ R::rgamma(hyp_tau21 + n/2, 1/(hyp_tau22 + 0.5 * arma::accu(sq2)) ) ;
-    // out_tau2(i+1) = out_tau2(i) ;
     
     
     /*
@@ -491,27 +492,22 @@ Rcpp::List calcium_gibbs(int Nrep,
 
     arma::vec out_slice_clusO = out_slice["clusterO"] ;
     clusterO.col(i+1) = out_slice_clusO ;
-    //clusterO.col(i+1) = clusterO.col(i) ;
     
     arma::vec out_slice_clusD = out_slice["clusterD"] ;
     clusterD.col(i+1) = out_slice_clusD ;
-//    clusterD.col(i+1) = clusterD.col(i) ;
     
     arma::vec out_slice_A = out_slice["A"] ;
     out_A.col(i+1) = out_slice_A ;
- //   out_A.col(i+1) = out_A.col(i) ;
     
 
     /*
-     * Sampling p (spike and slab proportion)
-     */
+    * Sampling p (spike and slab proportion)
+    */
     arma::vec line(n) ; line = clusterO.col(i+1) ;
     double n0 = std::count(line.begin(), line.end(), 0) ;
-    
+      
     out_p(i+1) = R::rbeta(hyp_p2 + n - n0, hyp_p1 + n0) ;
-    if(out_p(i+1) > 0.4) { check = 1 ; }
-    //   out_p(i+1) = out_p(i) ;
-
+    // if(out_p(i+1) > 0.4) { check = 1 ; }
     
     
     //// END Gibbs sampler ////
